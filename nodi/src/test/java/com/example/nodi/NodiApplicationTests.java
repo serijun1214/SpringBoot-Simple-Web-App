74300c6acd270e97008886b1ee2e4d24aae7929b
Junki Seri <junki.seri@rakus-partners.co.jp>
2023-10-25 10:30:51 +0900

    nodiプロジェクトを作成

---
+++ nodi/src/test/java/com/example/nodi/NodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
